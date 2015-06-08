1f69fcf854aad94862ae1bd22cdfac7a5fc57bc7
Justin Berry <jtberry1@gmail.com>
2015-06-08 22:30:50 +0900

    - Add basic robolectric unit test.

---
+++ app/src/test/java/androidproject/caniandroidnow/MainActivityTest.java
@@ -0,0 +1,24 @@
+package androidproject.caniandroidnow;
+
+import android.widget.TextView;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+  @Test
+  public void testSomething() {
+    MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+
+    TextView textView = (TextView) mainActivity.findViewById(R.id.textView);
+    Assert.assertThat(textView.getText().toString(), equalTo("Hello world!"));
+
+  }
+}
