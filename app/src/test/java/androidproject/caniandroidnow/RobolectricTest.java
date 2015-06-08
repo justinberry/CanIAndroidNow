0d25f905198f70f6eeeeca2d50d448b8382e8554
Justin Berry <jtberry1@gmail.com>
2015-06-09 00:02:56 +0900

    - Add robolectric superclass.

---
+++ app/src/test/java/androidproject/caniandroidnow/RobolectricTest.java
@@ -0,0 +1,9 @@
+package androidproject.caniandroidnow;
+
+
+import org.robolectric.annotation.Config;
+
+// TODO - see if there's a better spot to configure this.
+@Config(manifest = "src/main/AndroidManifest.xml")
+public class RobolectricTest {
+}
